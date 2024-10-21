b056bf74f75b7fd5c9b93cc97f40f5ed1fa1ccba
asifsarkar01 <aassiiff1155@gmail.com>
2024-10-21 11:44:48 -0500

    first react

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+  import './App.css'
+import Blogs from './component/blogs/Blogs'
+import Header from './component/headers/Header'
+
+  function App() {
+
+
+    return (
+      <>
+    
+  <Header></Header>
+  <Blogs></Blogs>
+
+      </>
+    )
+  }
+
+  export default App
---
+++ src/component/headers/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold text-center border-dashed underline">Knowledge Cafe-Here</h1>
+        </div>
+    );
+};
+
+export default Header;
